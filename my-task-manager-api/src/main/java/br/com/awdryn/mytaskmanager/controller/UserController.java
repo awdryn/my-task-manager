9996c7cf6cd98acbb7a8af0a6b62552364f50ce7
Awdryn Cavalache de Souza <awdryn@gmail.com>
2024-06-10 22:38:04 -0300

    Adicionado suporte a CORS

--- my-task-manager-api/src/main/java/br/com/awdryn/mytaskmanager/controller/UserController.java
+++ my-task-manager-api/src/main/java/br/com/awdryn/mytaskmanager/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.awdryn.mytaskmanager.dto.UserDTO;
 import br.com.awdryn.mytaskmanager.service.UserService;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
